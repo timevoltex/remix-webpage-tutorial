98c3bb6bc70ba88cbbfaf91b3a0df0fdaec6deb1
timevoltex <52909529+timevoltex@users.noreply.github.com>
2024-07-30 18:12:57 +0900

    git init

--- app/root.tsx
+++ app/root.tsx
@@ -1,53 +1,97 @@
+import {json} from "@remix-run/node";
+
 import {
-  Form,
-  Links,
-  Meta,
-  Scripts,
-  ScrollRestoration,
+    Form,
+    Link,
+    Links,
+    Meta,
+    Outlet,
+    Scripts,
+    ScrollRestoration,
+    useLoaderData,
 } from "@remix-run/react";
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+import {getContacts} from "./data";
 
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+import type {LinksFunction} from "@remix-run/node";
+
+import appStylesHref from "./app.css?url";
+
+export const links: LinksFunction = () => [
+    {rel: "stylesheet", href: appStylesHref}
+]
+
+export const loader = async () => {
+    const contacts = await getContacts();
+    return json({contacts});
+}
+
+
+export default function App() {
+    const {contacts} = useLoaderData<typeof loader>();
+
+    return (
+        <html lang="en">
+        <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <Meta/>
+            <Links/>
+        </head>
+        <body>
+        <div id="sidebar">
+            <h1>Remix Contacts</h1>
+            <div>
+                <Form id="search-form" role="search">
+                    <input
+                        id="q"
+                        aria-label="Search contacts"
+                        placeholder="Search"
+                        type="search"
+                        name="q"
+                    />
+                    <div id="search-spinner" aria-hidden hidden={true}/>
+                </Form>
+                <Form method="post">
+                    <button type="submit">New</button>
+                </Form>
+            </div>
+            <nav>
+                {contacts.length ? (
+                    <ul>
+                        {contacts.map((contact) => (
+                            <li key={contact.id}>
+                                <Link to={`/contacts/${contact.id}`}>
+                                    {contact.first || contact.last ? (
+                                        <>
+                                            {contact.first} {contact.last}
+                                        </>
+                                    ) : (
+                                        <i>
+                                            No Name
+                                        </i>
+                                    )}{" "}
+                                    {contact.favorite ? (
+                                        <span>*</span>
+                                    ) : null}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>
+                        <i>No Contacts</i>
+                    </p>
+                )}
+
+            </nav>
+        </div>
+        <div id="detail">
+            <Outlet/>
+        </div>
+        <ScrollRestoration/>
+        <Scripts/>
+        </body>
+        </html>
+    );
 }
